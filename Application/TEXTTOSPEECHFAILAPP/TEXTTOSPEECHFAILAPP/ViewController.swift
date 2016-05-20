7b01c12da9fab2121b30f4f858a08f9d8d06e5f2
Joris van de Wijgert <jvdwijgert@hotmail.com>
2016-05-20 14:50:32 +0200

    text to speech done
    i didn't fail completely
    but it's still stupid and useless

--- Application/TEXTTOSPEECHFAILAPP/TEXTTOSPEECHFAILAPP/ViewController.swift
+++ Application/TEXTTOSPEECHFAILAPP/TEXTTOSPEECHFAILAPP/ViewController.swift
@@ -7,9 +7,15 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var speechText: UITextView!
+    
+    let synth = AVSpeechSynthesizer()
+    var myUtterance = AVSpeechUtterance(string: "")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func textToSpeech(sender: AnyObject) {
+        myUtterance = AVSpeechUtterance(string: speechText.text)
+        //Snelheid
+        myUtterance.rate = 0.5
+        myUtterance.voice = AVSpeechSynthesisVoice(language:"en-US")
+        synth.speakUtterance(myUtterance)
+        myUtterance = AVSpeechUtterance(string: "")
+    }
+    
+    @IBAction func textZumSprechen(sender: AnyObject) {
+        myUtterance = AVSpeechUtterance(string: speechText.text)
+        //Snelheid
+        myUtterance.rate = 0.5
+        myUtterance.voice = AVSpeechSynthesisVoice(language:"de-DE")
+        synth.speakUtterance(myUtterance)
+        myUtterance = AVSpeechUtterance(string: "")
+    }
 
+    @IBAction func tekstNaarSpraak(sender: AnyObject) {
+        myUtterance = AVSpeechUtterance(string: speechText.text)
+        //Snelheid
+        myUtterance.rate = 0.5
+        myUtterance.voice = AVSpeechSynthesisVoice(language:"nl-NL")
+        synth.speakUtterance(myUtterance)
+        myUtterance = AVSpeechUtterance(string: "")
+    }
 }
 

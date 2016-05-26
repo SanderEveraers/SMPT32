b70f87761f909be64509287a6a1567824b171b32
Joris van de Wijgert <jvdwijgert@hotmail.com>
2016-05-26 11:37:09 +0200

    some small changes to sql and storyboard

--- Documents/SQL SCRIPT.sql
+++ Documents/SQL SCRIPT.sql
@@ -11,7 +11,7 @@ CREATE TABLE LEERLING
     ID		        INT				NOT NULL	AUTO_INCREMENT,
     Gebruikersnaam  VARCHAR(30)     NOT NULL,
     Wachtwoord      VARCHAR(64)     NOT NULL,
-    LaatstIngelogd  DATE            NOT NULL,
+    LaatstIngelogd  DATETIME        NOT NULL,
     
     CONSTRAINT UQ_LEERLING  UNIQUE(gebruikersnaam),
     
@@ -49,4 +49,55 @@ CREATE TABLE LEERLING_KLAS
     
     CONSTRAINT FK_LEERLINGKLASLEERLING  FOREIGN KEY (Leerling_ID)   REFERENCES LEERLING(ID),
     CONSTRAINT FK_LEERLINGKLASKLAS      FOREIGN KEY (Klas_ID)       REFERENCES KLAS(ID)
+);
+
+CREATE TABLE TOETS
+(
+    ID                  INT             NOT NULL    AUTO_INCREMENT,
+    Naam                VARCHAR(255)    NOT NULL,
+    Aanmaakdatum        DATETIME        NOT NULL,
+    Toetsdatum          DATETIME        NOT NULL,
+    Klas_ID             INT             NOT NULL,
+    
+    CONSTRAINT PK_TOETS         PRIMARY KEY (ID),
+    
+    CONSTRAINT FK_TOETSKLAS     FOREIGN KEY(Klas_ID)                REFERENCES KLAS(ID)
+);
+
+CREATE TABLE VRAAG
+(
+    ID                  INT             NOT NULL    AUTO_INCREMENT,
+    Vraag               VARCHAR(255)    NOT NULL,
+    Antwoord            VARCHAR(255)    NOT NULL,
+    ContextZin          VARCHAR(500)    NULL,
+    ContextAfb          VARCHAR(120)    NULL,
+    
+    CONSTRAINT PK_VRAAG         PRIMARY KEY (ID)
+);
+
+CREATE TABLE TOETS_VRAAG
+(
+    Toets_ID            INT             NOT NULL,
+    Vraag_ID            INT             NOT NULL,
+    
+    CONSTRAINT PK_TOETSVRAAG            PRIMARY KEY (Toets_ID, Vraag_ID),
+    
+    CONSTRAINT FK_TOETSVRAAGTOETS       FOREIGN KEY (Toets_ID)      REFERENCES TOETS(ID),
+    CONSTRAINT FK_TOETSVRAAGVRAAG       FOREIGN KEY (Vraag_ID)      REFERENCES VRAAG(ID)
+);
+
+CREATE TABLE VRAAG_BEANTWOORD
+(
+    ID                  INT             NOT NULL    AUTO_INCREMENT,
+    Leerling_ID         INT             NOT NULL,
+    Vraag_ID            INT             NOT NULL,
+    GegevenAntwoord     VARCHAR(255)    NOT NULL,
+    Tijdsduur           TIME            NOT NULL,
+    Datum               DATETIME        NOT NULL,
+    GoedBeantwoord      BOOLEAN          NOT NULL,
+    
+    CONSTRAINT PK_VRAAGBEANTWOORD       PRIMARY KEY (ID),
+    
+    CONSTRAINT FK_VBLEERLING            FOREIGN KEY (Leerling_ID)   REFERENCES LEERLING(ID),
+    CONSTRAINT FK_VBVRAAG               FOREIGN KEY (Vraag_ID)      REFERENCES VRAAG(ID)
 );

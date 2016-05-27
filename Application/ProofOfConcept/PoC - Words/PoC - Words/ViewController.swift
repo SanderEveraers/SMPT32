323b4d0b004114b7019df5eb27f1d8f3ecc051a7
StanWulms <fhict@MacBook-Pro-2.local>
2016-05-27 11:53:23 +0200

    PoC - Words - Swipe

--- Application/ProofOfConcept/PoC - Words/PoC - Words/ViewController.swift
+++ Application/ProofOfConcept/PoC - Words/PoC - Words/ViewController.swift
@@ -65,11 +65,50 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var leftSwipe = UISwipeGestureRecognizer(target: self, action: Selector("handleSwipes:"))
+        var rightSwipe = UISwipeGestureRecognizer(target: self, action: Selector("handleSwipes:"))
+        
+        leftSwipe.direction = .Left
+        rightSwipe.direction = .Right
+        
+        view.addGestureRecognizer(leftSwipe)
+        view.addGestureRecognizer(rightSwipe)
+    }
+    
+    func handleSwipes(sender:UISwipeGestureRecognizer) {
+        if (sender.direction == .Left) {
+            var index: Int = Int(arc4random_uniform(UInt32(words.count)))
+            var randomVal = Array(words.values)[index]
+            var bool = false
+            
+            while(!bool) {
+                if(randomVal == lbWoord.text) {
+                    index = Int(arc4random_uniform(UInt32(words.count)))
+                    randomVal = Array(words.values)[index]
+                } else {
+                    bool = true
+                }
+            }
+            lbWoord.text = ""
+            tbVertaling.text = ""
+            lbUitkomst.text = ""
+            lbWoord.text = randomVal
+        }
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func handleSwipe(recognizer:UIPanGestureRecognizer) {
+        let translation = recognizer.translationInView(self.view)
+        if let view = recognizer.view {
+            view.center = CGPoint(x:view.center.x + translation.x,
+                                  y:view.center.y + translation.y)
+        }
+        recognizer.setTranslation(CGPointZero, inView: self.view)
     }
 
 

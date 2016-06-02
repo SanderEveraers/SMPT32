1a2a6c26005281070ddc18bec5441c71457c4802
RobvanGastel <robvangastel@Robs-MacBook-Pro.local>
2016-06-02 14:19:19 +0200

    Updated controllers

--- Application/api/src/main/java/com/wordflip/api/SqlCreator.java
+++ Application/api/src/main/java/com/wordflip/api/SqlCreator.java
@@ -2,10 +2,12 @@ package com.wordflip.api;
 
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.Statement;
+import com.wordflip.api.models.Practice;
 
 import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.DateFormat;
 
 /**
  * Created by robvangastel on 02/06/16.
@@ -50,5 +52,62 @@ public class SqlCreator {
             }
         }
         return gotConnection;
+    }
+
+    public boolean validateUser(String userId) {
+
+        Connection con = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        boolean valid = false;
+
+        try {
+            con = (Connection) ds.getConnection();
+            stmt = (Statement) con.createStatement();
+            rs = stmt.executeQuery("select * FROM leerling WHERE ID =" + userId);
+
+            while(rs.next()){
+                valid = true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally{
+            try {
+                if(rs != null) rs.close();
+                if(stmt != null) stmt.close();
+                if(con != null) con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return valid;
+    }
+
+    public void addPractice(Practice practice, String userId) {
+
+        Connection con = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = (Connection) ds.getConnection();
+            stmt = (Statement) con.createStatement();
+            //java.sql.Date sqlDate = new java.sql.Date(practice.getDate().getTime());
+            //System.out.println(sqlDate.toString());
+
+            stmt.execute("INSERT INTO oefenmoment(Tijdstip, Tijdsduur, Leerling_ID, Aantal, Correct) VALUES ( " +
+                    "T'" + practice.getDuration() + "','" + userId + "','" +
+                    practice.getAmountOfWords() + "','" + practice.getCorrect() + "')");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally{
+            try {
+                if(rs != null) rs.close();
+                if(stmt != null) stmt.close();
+                if(con != null) con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }

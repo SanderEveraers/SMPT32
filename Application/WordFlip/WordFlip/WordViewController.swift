bc64c8cec55a4940e56aaa64bea084afcc15fb81
StanWulms <fhict@MacBook-Pro-2.local>
2016-06-07 19:47:23 +0200

    Added storyBoard with ui connections

---
+++ Application/WordFlip/WordFlip/WordViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WordViewController.swift
+//  WordFlip
+//
+//  Created by Fhict on 07/06/16.
+//  Copyright © 2016 FHICT. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import AVFoundation
+
+class WordViewController: UIViewController {
+
+    @IBOutlet weak var lbAantalWoorden: UILabel!
+    @IBOutlet weak var lbQuestion: UILabel!
+    @IBOutlet weak var lbAnswer: UILabel!
+    @IBOutlet weak var tbTranslation: UITextField!
+}

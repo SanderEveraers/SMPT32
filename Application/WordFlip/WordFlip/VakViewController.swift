c8f97d2d6c2eefabfe2971b7d690085461a39c21
Joris van de Wijgert <jvdwijgert@hotmail.com>
2016-06-23 10:00:19 +0200

    better colors

--- Application/WordFlip/WordFlip/VakViewController.swift
+++ Application/WordFlip/WordFlip/VakViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class VakViewController: UITableViewController {
+    
+    var difficulty:Int?
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
@@ -22,17 +24,17 @@ class VakViewController: UITableViewController {
         switch currentRow{
         case 0:
             cell.labelVakName?.text = "Engels"
-            cell.labelDificulty?.textColor = UIColor.greenColor()
+            cell.labelDificulty?.textColor = getColorFromDifficulty()
             cell.progressView.progress = 0.6
             cell.labelToets.text = "Toets hoofdstuk 4"
         case 1:
             cell.labelVakName?.text = "Frans"
-            cell.labelDificulty?.textColor = UIColor.redColor()
+            cell.labelDificulty?.textColor = UIColor.grayColor()
             cell.progressView.progress = 0.3
             cell.labelToets.text = "Toets hoofdstuk 3"
         case 2:
             cell.labelVakName?.text = "Duits"
-            cell.labelDificulty?.textColor = UIColor.orangeColor()
+            cell.labelDificulty?.textColor = UIColor.grayColor()
             cell.progressView.progress = 0.8
             cell.labelToets.text = "Toets hoofdstuk 6"
         default:
@@ -41,11 +43,36 @@ class VakViewController: UITableViewController {
         return cell
     }
     
+    func getColorFromDifficulty() -> UIColor
+    {
+        if difficulty != nil
+        {
+            switch difficulty! {
+            case 0:
+                return UIColor.grayColor()
+            case 1:
+                return UIColor.greenColor()
+            case 2:
+                return UIColor.yellowColor()
+            case 3:
+                return UIColor.orangeColor()
+            case 4:
+                return UIColor.redColor()
+            default:
+                return UIColor.grayColor()
+            }
+        }
+        else {
+            return UIColor.greenColor()
+        }
+    }
+    
     
     //Generated code:
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadJsonData()
         sleep(1)
         
         // Uncomment the following line to preserve selection between presentations
@@ -66,5 +93,36 @@ class VakViewController: UITableViewController {
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return 3
+    }
+    
+    func loadJsonData()
+    {
+        let url = NSURL(string: "\(api.url)/toetsrating?toetsid=1")
+        let request = NSURLRequest(URL: url!)
+        let session = NSURLSession.sharedSession()
+        let dataTask = session.dataTaskWithRequest(request) { (data, response, error) -> Void in
+            do
+            {
+                if let jsonObject: AnyObject = try NSJSONSerialization.JSONObjectWithData(data!, 	   options: NSJSONReadingOptions.AllowFragments)
+                {
+                    self.parseJSONData(jsonObject)
+                }
+            }
+            catch
+            {
+                print("Error parsing JSON data")
+            }
+        }
+        dataTask.resume();
+    }
+    
+    func parseJSONData(jsonObject:AnyObject){
+        if let jsonData = jsonObject as? NSObject
+        {
+            let level = jsonData.valueForKey("rating") as? NSInteger
+            if (level != nil){
+                difficulty = level
+            }
+        }
     }
 }
